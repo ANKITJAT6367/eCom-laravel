bd4097c766b8edef9e3968b62b7311a61fa67f28
Ankitjat6367 <ankitjaaat2003@gmail.com>
2025-05-29 11:42:19 +0530

    login done

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\UserAuth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', function () {
